29acd4b67b1c24172a3b01fb2c3da4a7ed91a02f
Raul <raul_cosmin_26@yahoo.com>
2024-09-22 22:03:56 +0300

    Highlight active page.

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,10 +1,18 @@
+"use client";
 import Link from "next/link";
 import { MenuItemProps } from "@/types/index";
+import { usePathname } from "next/navigation";
+import { clsx } from "clsx";
 
 export default function MenuItem({ children, link }: Readonly<MenuItemProps>) {
+  const pathname = usePathname();
+
   return (
     <li>
-      <Link href={link} className="menu-item">
+      <Link
+        href={link}
+        className={clsx("menu-item", link === pathname && "menu-item--active")}
+      >
         {children}
       </Link>
     </li>
--- components/MobileMenuItem.tsx
+++ components/MobileMenuItem.tsx
@@ -1,10 +1,20 @@
+"use client";
 import Link from "next/link";
 import { MenuItemProps } from "@/types/index";
+import { clsx } from "clsx";
+import { usePathname } from "next/navigation";
 
 export default function MenuItem({ children, link }: Readonly<MenuItemProps>) {
+  const pathName = usePathname();
   return (
     <li>
-      <Link href={link} className="mobile-menu-item">
+      <Link
+        href={link}
+        className={clsx(
+          "mobile-menu-item",
+          pathName === link && "mobile-menu-item--active",
+        )}
+      >
         {children}
       </Link>
     </li>
